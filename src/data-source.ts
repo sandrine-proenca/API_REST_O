6747680f5327bbfa9c192537450dce5bf72516a5
TB <terry.boeuf@hotmail.fr>
2023-01-23 14:32:10 +0100

    add : creation dossiers, data-src

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+require ('dotenv').config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "Petit-dragon31",
-    database: "API_REST_O",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: ["src/entity/**/*.ts"],
     migrations: [],
     subscribers: [],
 })
