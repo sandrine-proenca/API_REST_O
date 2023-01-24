bbce5ab51d35bbc92024db10b12eda1a90127c09
TB <terry.boeuf@hotmail.fr>
2023-01-24 15:19:03 +0100

    test synchro

---
+++ src/controllers/menuController.ts
@@ -0,0 +1,200 @@
+import { Request, Response } from "express";
+import { MenuService } from "../services/menuService";
+
+const menuService = new MenuService()
+
+/**
+ * Class permettant le contrôle des données entrantes pour les requête menus
+ * * **.getMenus()** : Contrôle préalable à la récupération de tous les menus
+ * * **.getMenuId()** : Contrôle préalable à la récupération d'un menu grâce à son id
+ * * **.postMenu()** : Contrôle préalable à l'ajout d'un nouveau menu
+ * * **.updtateMenu()** : Contrôle préalable à la modification d'un menu
+ * * **.deleteMenu()** : Contrôle préalable à la suppression d'un menu
+ */
+export class MenuController {
+
+
+    /**
+        * Contrôle préalable à la récupération de tous les menus
+        */
+    async getMenus(req: Request, res: Response) {
+        try {
+            const data = await menuService.getAllMenus();
+            res.status(200).json(
+                {
+                    status: "success",
+                    message: "Liste des menus",
+                    data: data
+                }
+            )
+        }
+        catch (err: any) {
+            res.status(500).json(
+                {
+                    status: "Fail",
+                    message: "erreur serveur"
+                }
+            )
+            console.log(err.stack)
+        }
+    }
+
+
+
+    /**
+        * Contrôle préalable à la récupération d'un menu grâce à son id
+        */
+    async getMenuId(req: Request, res: Response) {
+        const menuId = parseInt(req.params.id);
+        if (!Number.isNaN(menuId)) {
+            try {
+                const data = await menuService.getMenuById(menuId)
+
+                if (data) {
+                    res.status(200).json(
+                        {
+                            status: "success",
+                            data: data
+                        }
+                    )
+                }
+                else {
+                    res.status(404).json(
+                        {
+                            status: "fail",
+                            message: "l'ID ne correspond a aucun menu"
+                        }
+                    )
+                }
+            }
+            catch (err: any) {
+                res.status(500).json(
+                    {
+                        status: "fail",
+                        message: "erreur serveur"
+                    }
+                )
+                console.log(err.stack);
+            }
+        }
+        else {
+            res.status(404).json(
+                {
+                    status: "fail",
+                    message: "numero d'ID necessaire"
+                }
+            )
+        }
+    }
+
+
+
+    /**
+     * Controle prealable a l'ajout d'un nouveau menu
+     */
+    async postMenu(req: Request, res: Response) {
+        const name: string = req.body.name
+        const price: string = req.body.price
+        const admin: boolean = req.body.admin
+
+        if (name && price && admin) {
+            try {
+                const data = await menuService.postMenu(name, price)
+                res.status(201).json(
+                    {
+                        status: "success",
+                        message: "menu ajoute avec succes",
+                        data: data
+                    }
+                )
+            }
+            catch (err: any) {
+                res.status(500).json(
+                    {
+                        status: "fail",
+                        message: "erreur serveur"
+                    }
+                )
+                console.log(err.stack);
+            }
+        }
+        else {
+            res.status(400).json(
+                {
+                    status: "fail",
+                    message: "nom, prix ou droits admin manquant"
+                }
+            )
+        }
+    }
+
+
+    /**
+     *Contrôle préalable à la suppression d'un menu
+      */
+    async deleteMenu(req: Request, res: Response) {
+        const menuId = parseInt(req.params.id)
+        const userId: number = req.body.userId
+        const admin: boolean = req.body.admin
+
+        if (!Number.isNaN(menuId)) {
+            try {
+                const menuData = await menuService.getMenuById(menuId)
+
+                if (!menuData) {
+                    res.status(404).json(
+                        {
+                            status: "fail",
+                            message: "l'ID ne correspond a aucun menu"
+                        }
+                    )
+                }
+                else if (userId !== menuData.id && !admin) {
+                    res.status(400).json(
+                        {
+                            status: "fail",
+                            message: "action non-autorisee"
+                        }
+                    )
+                }
+                else {
+                    const data = await menuService.deleteMenu(menuId)
+                    if (data) {
+                        res.status(200).json(
+                            {
+                                status: "success",
+                                message: "article supprime"
+                            }
+                        )
+                    }
+                }
+            }
+            catch (err: any) {
+                res.status(500).json(
+                    {
+                        status: "fail",
+                        message: "erreur serveur"
+                    }
+                )
+                console.log(err.stack);
+
+            }
+        }
+        else {
+            res.status(404).json(
+                {
+                    status: "fail",
+                    message: "ID necessaire"
+                }
+            )
+        }
+    }
+
+
+   /**
+    *Contrôle préalable à la modification d'un menu
+     */
+    async updateMenu(req:Request, res:Response) {
+        const menuId=parseInt
+    }
+}
---
+++ src/services/menuService.ts
@@ -0,0 +1,84 @@
+import { Menu } from "../entity/Menu";
+
+
+
+/**
+ * Class permettant la gestion des requètes SQL pour les menus
+ * * **.getAllMenus()** : recupère tout les menus de la BDD
+ * * **.getMenuById()** : recupère un menu par son id de la BDD
+ * * **.postmenu()** : ajoute un nouveau menu à la BDD
+ * * **.updateMenu()** : modifie un menu par son id de la BDD
+ * * **.deleteMenu()** : supprime un menu de la BDD
+ */
+export class MenuService {
+
+
+
+    /**
+        * recupère tout les menus de la BDD 
+        */
+    async getAllMenus() {
+        const menus = await Menu.find();
+
+        return menus;
+    }
+
+
+
+    /**
+     * recupère un menu par son id de la BDD
+     */
+    async getMenuById(id: number) {
+        const menu = await Menu.findBy({ id: id });
+        console.log(menu);
+        return menu[0]
+    }
+
+
+
+    /**
+     * ajoute un nouveau menu à la BDD
+     */
+    async postMenu(name: string, price: string) {
+
+        const menu = new Menu()
+        menu.name = name
+        menu.price = price
+
+        await menu.save()
+
+        return menu
+    }
+
+
+
+    /**
+     * supprime un menu de la BDD
+     */
+    async deleteMenu(id: number): Promise<Menu | undefined> {
+        const menu = this.getMenuById(id);
+
+        (await menu).remove()
+
+        console.log(menu);
+        return menu
+
+    }
+
+    
+
+    /**
+     * modifie un menu par son id de la BDD
+     */
+    async updateMenu(updateName: string, UpdatePrice: string, id: number) {
+        const menu = await this.getMenuById(id);
+        menu.name = updateName
+        menu.price = UpdatePrice
+
+        Menu.save(menu)
+
+        return menu
+
+    }
+
+}
