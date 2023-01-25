e26c8ec39e85f3e33486477219b89257d0f79c69
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-25 10:07:39 +0100

    comentaire de userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,23 +2,30 @@ import { DataSource } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User"
 
+/**
+ * Class permetant la gestion de requètes sql pour les users
+ * **.addUser()**: ajoute un nouveau user
+ * **.getUserByEmail**: récupère un user avec son email
+ */
 export class UserService
 {
-
+    /**
+     * ajoute un nouveau user dans la BDD
+     */
     async addUser(email: string, hash: string): Promise<User | undefined>
     {
         const user = new User()
+
         user.email = email
         user.password = hash
-        await user.save()
-        console.log('------- service --------- test 1 ---------------', AppDataSource.options.entities);
-        //////////////////////////////
 
-        
+        await user.save()
         
         return user;
     }
-
+    /**
+     * récupère un user avec son email dans la BDD
+     */
     async getUserByEmail(email: string, hash: string): Promise <User | undefined> {
 
         const user = new User()
@@ -29,11 +36,4 @@ export class UserService
 
         return user
     }
-    
-    /* static findByEmail(email: string, password: string){
-        return this.createQueryBuilder("user")
-            .where("user.email = :email", {email})
-            .andWhere("user.password = :password", {password})
-            .getMany()
-    } */
 }
