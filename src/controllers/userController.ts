81588ee7d1e9c88d519000482ee48d8cb7bf2c7b
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-25 11:30:53 +0100

    création du login dans controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,53 +1,56 @@
 import { Request, Response } from "express";
 import { UserService } from "../services/userService";
 import * as bcrypt from 'bcrypt';
-import { AppDataSource } from "../data-source";
-import { User } from "../entity/User";
+import * as jwt from 'jsonwebtoken';
 
-
+/**
+ * Appel de Class permetant la gestion de requètes sql pour les users
+ */
 const userService = new UserService();
-
+/**
+ * Class qui fait le contrôle préalable des données pour les users
+ * **.register()**: Contrôle préalable de la création d'un user
+ * **.login()**: contrôle préalable de la récupération d'un user par son email
+ */
 export class UserController
 {
-
+    /**
+     * Contrôle préalable de la création d'un user
+     */
     async register(req: Request, res: Response)
     {
 
         const { email, password } = req.body
-        console.log('------ controller ---------- test 111 ----- req.body ----------',req.body);
-
-        /* if (email) {
-            res.status(400).json({
-                status: "FAILED",
-                message: "This email already exists, change it",
-                data: undefined
-            });
-            console.log('------ controller ---------- test 222 ---- email déjà existant -----------');
-            return;
-        }; */
-
-        /* if (!email || !password)
+        //message d'erreur pour email ou password incorect
+        if (!email || !password)
         {
             res.status(400).json({
                 status: "FAILED",
                 message: "error in email or password",
                 data: undefined
             });
-            console.log('------ controller ---------- test 333 ----- email ou password incorect ----------');
             return;
-        } */
+        }
+        /**
+         * vérification que le user n'existe pas déjà dans la BDD
+         */
+        const user = await userService.getUserByEmail(email, password);
+        if (user){
+            res.status(400).json({
+                status: "FAILED.",
+                message: "This user already exists, please change your name"
+            });
+            return;
+        }
+        /**hachage du mot de passe */
         bcrypt.hash(password, 10, async (err, hash) =>
         {
-
             try
             {
-                console.log('------ controller ---------- test 444 ------ début du TRY ---------', userService);
-//////////////////////////////////////////
-
+                /**
+                 * ajout du nouveau user dans la BDD
+                 */
                 const user = await userService.addUser(email, hash);
-                console.log('------ controller ---------- test 555 --- appel du addUser ------------');
-
-
                 res.status(201).json({
                     status: "OK",
                     message: `The name: ${email} and the associated password have been successfully created.`,
@@ -61,8 +64,82 @@ export class UserController
                     data: undefined
                 })
                 console.log(err);
-
             }
         })
+    }
+    /**
+     * contrôle préalable de la récupération d'un user par son email
+     */
+    async login(req: Request, res: Response)
+    {
+        const { email, password } = req.body;
+
+        //vérifier que l'email existe et bien écrit
+        if (email === undefined || typeof email !== typeof String())
+        {
+            res.status(403).json({
+                status: "FAILED",
+                message: "This email is invalid",
+                data: undefined
+            })
+            return;
+        }
+
+        //vérifier  que le password existe et bien écrit
+        if (password === undefined || typeof password !== typeof String())
+        {
+            res.status(403).json({
+                status: "FAILED.",
+                message: "This password is invalid",
+                data: undefined
+            })
+            return;
+        }
+        try
+        {
+            /**
+             * récupération du user s'il existe dans la BDD
+             */
+            const user = await userService.getUserByEmail(email, password);
+
+            if (user)
+            {
+                /**hachage du password donné pour comparaison avec celui déjà enregistré */
+                bcrypt.compare(password, user.password, function (err, result)
+                {
+                    const accessToken = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET!);
+
+                    if (result === true)
+                    {
+                        res.status(200).json({
+                            status: "OK.",
+                            message: "This password is valid",
+                            data: accessToken
+                        })
+                    }
+                    else
+                    {
+                        res.status(403).json({
+                            status: "FAILED.",
+                            message: "This password is invalid",
+                            data: undefined
+                        });
+                    }
+                })
+            }
+            else {
+                res.status(403).json({
+                    status: "FAILED.",
+                    message: "This user don't exist",
+                    data: undefined
+                });
+            }
+        }
+        catch(err){
+            res.status(500).json({
+                status: "FAILED.",
+                message: "Internal Server Error"
+            })
+        }
     }
 }
