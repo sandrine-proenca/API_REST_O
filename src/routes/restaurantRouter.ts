ec0958cd2c67aff21fbe781ff68b978c0553ff15
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-01-26 12:22:27 +0100

    merge

--- src/routes/restaurantRouter.ts
+++ src/routes/restaurantRouter.ts
@@ -3,10 +3,15 @@ import { RestaurantController } from '../controllers/restaurantController';
 
 export const restaurantRouter = express.Router()
 
+/**
+ * appel de la class RestaurantController
+ */
 const restaurantController = new RestaurantController()
 
+/**création d'un nouveau restaurant par un admin dans la BDD */
 restaurantRouter.post('/', restaurantController.postRestaurant)
 
+/**récupération de tous les restaurants par un user */
 restaurantRouter.get('/', restaurantController.getRestaurant)
 
 restaurantRouter.get('/:id', restaurantController.getOneRestaurant)
